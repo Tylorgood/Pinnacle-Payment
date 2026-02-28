7f6270252bd8a3c8575a58885c9d01bf19f98a84
journi <journi@email.com>
2026-02-27 23:12:20 -0500

    Fix fs not defined error

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 const axios = require('axios');
 
 const app = express();
